0e2efab1d5006df7157f041677ea13196c72a57f
Vidya Ganapati <vganapati@gmail.com>
2024-10-01 13:08:44 -0700

    initial files

---
+++ initial_script.py
@@ -0,0 +1,24 @@
+from huggingface_hub import login
+from esm.models.esm3 import ESM3
+from esm.sdk.api import ESM3InferenceClient, ESMProtein, GenerationConfig
+
+# Will instruct you how to get an API key from huggingface hub, make one with "Read" permission.
+# login() # only needed to fetch the model from Hugging Face Hub
+
+# This will download the model weights and instantiate the model on your machine.
+model: ESM3InferenceClient = ESM3.from_pretrained("esm3_sm_open_v1").to("cuda") # or "cpu"
+
+# Generate a completion for a partial Carbonic Anhydrase (2vvb)
+prompt = "___________________________________________________DQATSLRILNNGHAFNVEFDDSQDKAVLKGGPLDGTYRLIQFHFHWGSLDGQGSEHTVDKKKYAAELHLVHWNTKYGDFGKAVQQPDGLAVLGIFLKVGSAKPGLQKVVDVLDSIKTKGKSADFTNFDPRGLLPESLDYWTYPGSLTTPP___________________________________________________________"
+protein = ESMProtein(sequence=prompt)
+# Generate the sequence, then the structure. This will iteratively unmask the sequence track.
+protein = model.generate(protein, GenerationConfig(track="sequence", num_steps=8, temperature=0.7))
+# We can show the predicted structure for the generated sequence.
+protein = model.generate(protein, GenerationConfig(track="structure", num_steps=8))
+protein.to_pdb("./generation.pdb")
+# Then we can do a round trip design by inverse folding the sequence and recomputing the structure
+protein.sequence = None
+protein = model.generate(protein, GenerationConfig(track="sequence", num_steps=8))
+protein.coordinates = None
+protein = model.generate(protein, GenerationConfig(track="structure", num_steps=8))
+protein.to_pdb("./round_tripped.pdb")
